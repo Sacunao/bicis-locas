b2291b5580b93f6d552f7bbd474a92724dba2f88
sacunao <sheila.aobregon@gmail.com>
2016-09-15 11:02:07 -0500

    validando campos obligatorios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	/* validar campo obligatorio en nombre, apellido, correo y contraseña */
+    var aviso = alert("Por favor llena los campos obigatorios: Nombre, apellido, correo, contraseña y bicicleta");
+    
+    var valor = document.getElementById("name").value;
+    if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+        return aviso;
+    }
+    var valorDos = document.getElementById("lastname").value;
+    if( valorDos == null || valorDos.length == 0 || /^\s+$/.test(valorDos) ) {
+        return aviso;
+    }
+    var valorTres = document.getElementById("input-email").value;
+    if( valorTres == null || valorTres.length == 0 || /^\s+$/.test(valorTres) ) {
+       return aviso;
+    }
+    var valorCuatro = document.getElementById("input-password").value;
+    if( valorCuatro == null || valorCuatro.length == 0 || /^\s+$/.test(valorCuatro) ) {
+        return aviso;
+    }
+    var valorCinco = document.getElementById("input-social").value;
+    if( valorCinco == null || valorCinco .length == 0 || /^\s+$/.test(valorCinco ) ) {
+        return aviso;
+    }
+    
 }
