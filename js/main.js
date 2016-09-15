c51b2d7a6307b2b20e18fc2dfdde15e02d3504e7
sacunao <sheila.aobregon@gmail.com>
2016-09-15 11:38:39 -0500

    validando minimo 6 caracteres de contraseña

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 function validateForm(){
 	/* validar campo obligatorio en nombre, apellido, correo y contraseña */
-    var aviso = alert("Por favor aegúrate de haber llenado los campos obligatorios: \n Nombre, Apellido, Correo, Contraseña y Bicicleta");
+    //var aviso = alert("Por favor aegúrate de haber llenado correctamente los campos obligatorios: \n Nombre, Apellido, Correo, Contraseña y Bicicleta");
     
     var valor = document.getElementById("name").value;
     if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-        return aviso;
-    }
+        return alert("Ingresa un nombre");
+    } 
     var valorDos = document.getElementById("lastname").value;
     if( valorDos == null || valorDos.length == 0 || /^\s+$/.test(valorDos) ) {
-        return aviso;
+        return alert("Ingresa un apeliido");
     }
     var valorTres = document.getElementById("input-email").value;
-    if( valorTres == null || valorTres.length == 0 || /^\s+$/.test(valorTres) ) {
-       return aviso;
+    if( valorTres == null || valorTres.length == 0 || !/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/.test(valorTres)) {
+       return alert("Ingresa un correo");
     }
     var valorCuatro = document.getElementById("input-password").value;
     if( valorCuatro == null || valorCuatro.length == 0) {
-        return aviso;
-    } if ( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor)) ){
-        return alert("Ingresa un correo válido");
+        return alert("Ingresa una contraseña");
+    } else if (valorCuatro.length < 6){
+        return alert("tu contraseña debe tener más de 6 carácteres")
     }
     var valorCinco = document.getElementById("input-social").value;
     if( valorCinco == null || valorCinco .length == 0 || /^\s+$/.test(valorCinco ) ) {
-        return aviso;
+        return alert("elige una opcion");
     }
     
 
