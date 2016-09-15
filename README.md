bab92628f2c03d2482b44065ac605753c812a552
sacunao <sheila.aobregon@gmail.com>
2016-09-15 12:30:54 -0500

    modificando readme e indentacion

--- README.md
+++ README.md
@@ -4,10 +4,17 @@
 3. Crea un repositorio que se llame **bicis-locas** en GITHUB, haz tu primer commit y sube los archivos al repositorio. 
 4. Abre el archivo **js/main.js** y completa la función **validateForm**, se deben hacer las siguientes validaciones en los campos:
 ⋅⋅* Todos los campos son obligatorios, excepto los dos últimos. 
+Para validar los elementos obligatorios se uso la siguiente condicion: ( valor == null || valor.length == 0 || /^\s+$/.test(valor).
+
 ..* Los campos nombre y apellido sólo deben permitir caracteres de la A-Z
 ..* Para los campos nombre y apellido la primera letra debe ser mayúscula
+
 ..* Validar que el campo email tenga un formato válido. Ej: name@domain.com
+Para validar los correos se aumento lo siguiente en la condicion ya cread: !/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/.test(valorTres)).
+
 ..* El campo password debe tener al menos 6 caracteres
 ..* El campo password no puede ser igual a "password" ó "123456" ó "098754"
+Se uso la siguiente condicion (valorCuatro == null || valorCuatro.length == 0 || valorCuatro.length <= 6 || valorCuatro == "123456" || valorCuatro == "98754" || valorCuatro == "password")
+
 ..* El valor seleccionado de bicis, debe ser una de las opciones presentadas
 4. Si algún campo presenta error debe informarse al usuario por medio de un alert.
--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 function validateForm(){
 	
     var valor = document.getElementById("name").value;
-    if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+    if(valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
         return alert("Ingresa un nombre");
     } 
     var valorDos = document.getElementById("lastname").value;
-    if( valorDos == null || valorDos.length == 0 || /^\s+$/.test(valorDos) ) {
+    if(valorDos == null || valorDos.length == 0 || /^\s+$/.test(valorDos) ) {
         return alert("Ingresa un apeliido");
     }
     var valorTres = document.getElementById("input-email").value;
-    if( valorTres == null || valorTres.length == 0 || !/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/.test(valorTres)) {
+    if(valorTres == null || valorTres.length == 0 || !/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/.test(valorTres)) {
        return alert("Ingresa un correo");
     }
     var valorCuatro = document.getElementById("input-password").value;
-    if( valorCuatro == null || valorCuatro.length == 0 || valorCuatro.length <= 6 || valorCuatro == "123456" || valorCuatro == "98754" || valorCuatro == "password") {
+    if(valorCuatro == null || valorCuatro.length == 0 || valorCuatro.length <= 6 || valorCuatro == "123456" || valorCuatro == "98754" || valorCuatro == "password") {
         return alert("Contraseña no válida");
     }
     var valorCinco = document.getElementById("input-social").value;
-    if( valorCinco == null || valorCinco .length == 0 || /^\s+$/.test(valorCinco ) ) {
+    if(valorCinco == null || valorCinco .length == 0 || /^\s+$/.test(valorCinco ) ) {
         return alert("elige una opcion");
     }
 }
